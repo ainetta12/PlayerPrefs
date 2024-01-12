b1afb4597f81d4a4ff85e59cc1b0e36ca4f32a33
ainetta12 <ainasanzg@gmail.com>
2024-01-12 10:02:22 +0100

    datos

--- Assets/Script/SaveManager.cs
+++ Assets/Script/SaveManager.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveManager : MonoBehaviour
 {
+    [SerializeField] private Text userNameText;   
+    [SerializeField] private Text userScoreText;
+    [SerializeField] private Text userPositionText;
+
+   
+    [SerializeField] private string userName;
+    [SerializeField] private int userScore;
+    [SerializeField] private Vector3 userPosition;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadData();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveSata()
     {
-        
+        PlayerPrefs.SetString("name", userName);    
+        PlayerPrefs.SetInt("score", userScore);
+        PlayerPrefs.SetFloat("positionX", userPosition.x);
+        PlayerPrefs.SetFloat("positionY", userPosition.y);
+        PlayerPrefs.SetFloat("positionZ", userPosition.z);
+
+        LoadData();
     }
+
+    void LoadData()
+    {
+        userNameText.text = "User name:" + PlayerPrefs.GetString("name", "No name");
+        userScoreText.text = "User score:" + PlayerPrefs.GetInt("score", 0).ToString();
+        userPositionText.text = "Player position:" + PlayerPrefs.GetFloat("positionX", 0).ToString() + "x" +
+                                PlayerPrefs.GetFloat("positionY", 0).ToString() + "y" + 
+                                PlayerPrefs.GetFloat("positionZ", 0).ToString() + "z";
+    }
+
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteKey("name");
+        PlayerPrefs.DeleteKey("score");
+        PlayerPrefs.DeleteKey("psotionX");
+        PlayerPrefs.DeleteKey("psotionY");
+        PlayerPrefs.DeleteKey("psotionZ");
+
+        PlayerPrefs.DeleteAll();
+
+        LoadData();
+    }
+
 }
